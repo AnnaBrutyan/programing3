625dfbbe35005794701221e632dcc440298f657a
EdgarTovmasyan <edgar.tovmasyan2@tumo.org>
2023-05-07 12:51:59 +0400

    Create project

--- grass.js
+++ grass.js
@@ -34,7 +34,7 @@ class Grass {
         this.multiply++;
         var newCell = random(this.chooseCell(0));
 
-        
+
         if (this.multiply >= 2 && newCell) {
             var newGrass = new Grass(newCell[0], newCell[1]);
             grassArr.push(newGrass);
